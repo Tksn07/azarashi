bdff2eb5cca209598e1fb1987c26b2c1640d8eac
Fuji Taka <exia.hinata0622@gmail.com>
2023-07-25 02:27:31 +0900

    icon追加

---
+++ src/components/Icon/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Icon } from './index'
+
+// コンポーネントの概要
+export default {
+  title: 'UI/Icon',
+  component: Icon,
+}
+
+export const IconStry = () => <Icon />
---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,10 @@
+import { MdHome } from "react-icons/md";
+
+interface Props {
+}
+
+export const Icon = ({  }:Props) => {
+  return (
+    <div><MdHome /></div>
+  )
+}
